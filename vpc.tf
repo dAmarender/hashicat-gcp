c64458cba191ed809cd5be52eda0955fb6cc65be
dAmarender <dodleamarenderreddy@gmail.com>
2022-09-26 03:03:11 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source  = "terraform-google-modules/network/google//modules/subnets"
+    source  = "terraform-google-modules/network/google"
     version = "~> 3.4.0"
 
     project_id   = var.project
