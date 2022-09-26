af058dd7af2dcb749167f3067d3ba3e8604e7753
dAmarender <dodleamarenderreddy@gmail.com>
2022-09-26 02:58:03 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,9 @@
 module "vpc" {
     source  = "terraform-google-modules/network/google//modules/subnets"
-    version = "~> 3.4.0"
+    version = "~> 4.0"
 
     project_id   = var.project
-    network_name = "gaurav-network"
+    network_name = "gau-network"
 
     subnets = [
         {
