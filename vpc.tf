7a159cf459b5c06cf29ad0a3d598da9d3c0e665f
dAmarender <dodleamarenderreddy@gmail.com>
2022-09-26 03:43:37 +0000

    added vpc module

--- vpc.tf
+++ vpc.tf
@@ -3,22 +3,32 @@ module "vpc" {
     version = "~> 3.4.0"
 
     project_id   = var.project
-    network_name = "gaurav-network"
+    network_name = "dmarender-netword"
 
     subnets = [
         {
-            subnet_name           = "gaurav-subnet"
+            subnet_name           = "dmarender-network01"
             subnet_ip             = "10.10.10.0/24"
             subnet_region         = var.region
         },
         {
-            subnet_name           = "gau-subnet-02"
+            subnet_name           = "d-subnet-02"
             subnet_ip             = "10.10.20.0/24"
             subnet_region         = var.region
             subnet_private_access = "true"
             subnet_flow_logs      = "true"
             description           = "This subnet has a description"
         },
+        {
+            subnet_name                  = "d-subnet-03"
+            subnet_ip                    = "10.10.30.0/24"
+            subnet_region                = var.region
+            subnet_flow_logs             = "true"
+            subnet_flow_logs_interval    = "INTERVAL_10_MIN"
+            subnet_flow_logs_sampling    = 0.7
+            subnet_flow_logs_metadata    = "INCLUDE_ALL_METADATA"
+            subnet_flow_logs_filter_expr = "true"
+        }
     ]
 
     secondary_ranges = {
